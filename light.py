6343f388bf0d5a110cc1d60897a8281eab261b44
robbrad <robbrad182@hotmail.com>
2023-12-13 18:43:19 +0000

    init commit

--- light.py
+++ light.py
@@ -28,7 +28,7 @@ from homeassistant.const import (
 )
 from homeassistant.core import HomeAssistant
 from homeassistant.helpers import device_registry as dr
-from homeassistant.helpers.device_registry import DeviceInfo
+from homeassistant.helpers.entity import DeviceInfo
 from homeassistant.helpers.entity_platform import AddEntitiesCallback
 
 from .const import (
